e362de23d1f194a6c5cd543468737ad354f9d087
Arthur Neves <arthurnevesx@hotmail.com>
2022-05-21 19:45:11 -0300

    Instalando e configurando react-router-dom

--- hiring-coders/react-basico/gamagit/src/index.js
+++ hiring-coders/react-basico/gamagit/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App title="Hello" user="Gama Academy" />
+    <App />
   </React.StrictMode>
 );
 
