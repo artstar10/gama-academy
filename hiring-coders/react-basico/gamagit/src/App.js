a4e2c5fdb66d301740d18a9f1296052db1f4ec4d
Arthur Neves <arthurnevesx@hotmail.com>
2022-05-21 17:09:09 -0300

    inserindo button

--- hiring-coders/react-basico/gamagit/src/App.js
+++ hiring-coders/react-basico/gamagit/src/App.js
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 
 function App(props) {
-  const [usuario, setUsuario ] = useState('Arthur');
   return (
     <> 
-    <p>{usuario}</p>
       <input name='usuario' id='usuario' className='usuarioInput' placeholder='Usuário'/>
+      <button type="button">Pesquisar</button>
     </>
   );
 }
