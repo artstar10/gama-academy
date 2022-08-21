5ba07d83a9c533c392e769ea27d9beacbc2b3a5c
Arthur Neves <arthurnevesx@hotmail.com>
2022-08-21 15:02:26 -0300

    operadores aritméticos

---
+++ dev-for-tech/back-end-nodejs/javascript/operadores.js
@@ -0,0 +1,24 @@
+// operadores aritméticos
+
+// adição +
+var somarDoisNumeros = 1 + 3;
+console.log(somarDoisNumeros);
+//subtração -
+var subtrairDoisNumeros = 1 - 3;
+console.log(subtrairDoisNumeros);
+//multiplicação *
+var multiplicarDoisNumeros = 1 * 3;
+console.log(multiplicarDoisNumeros);
+//divisão /
+var dividirDoisNumeros = 4 / 2;
+console.log(dividirDoisNumeros);
+//módulo ou resto de uma divisão -
+console.log(8%3);
+// incremento ++
+var numero = 1;
+console.log(++numero);
+numero = numero + 1
+// decremento --
+var numero = 2;
+console.log(--numero);
+numero = numero - 1
