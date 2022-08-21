ce7e57cac359414978583609d211cdde2f51dd43
Arthur Neves <arthurnevesx@hotmail.com>
2022-08-21 14:56:17 -0300

    tipos de dados

---
+++ dev-for-tech/back-end-nodejs/javascript/tipo-de-dados.js
@@ -0,0 +1,24 @@
+var meuNome = 'Arthur'; // string
+
+var age  = 26 // number
+
+var altura = 1.87 // number
+
+var isStudent = false /// bboolean
+
+// Objeto, array e function
+var pessoa = {
+    "nome": "Arthur",
+    "idade": 26
+}
+
+var estudantes = ["Arthur", "Maria", "João"]
+
+function soma(a,b) {
+    return a + b
+}
+
+//operador typeof
+
+var isStudent = true
+console.log(typeof isStudent);
