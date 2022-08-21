7da1737b500aa0dcb6912d0878d747390ee85670
Arthur Neves <arthurnevesx@hotmail.com>
2022-08-21 15:12:42 -0300

    condicionais

---
+++ dev-for-tech/back-end-nodejs/javascript/condicionais.js
@@ -0,0 +1,11 @@
+var numero =1;
+numero = 5;
+
+if(numero === 1){
+    console.log("numero é igual a 1");
+}else if(numero === 2){
+    console.log("numero é igual a 2");
+}else{
+    console.log("numero é 5");
+}
+
