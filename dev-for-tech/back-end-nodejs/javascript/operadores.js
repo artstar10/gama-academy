f92630494374fff97f4b2498424a18fe49c60cbd
Arthur Neves <arthurnevesx@hotmail.com>
2022-08-21 15:06:12 -0300

    operadores de atribuição

--- dev-for-tech/back-end-nodejs/javascript/operadores.js
+++ dev-for-tech/back-end-nodejs/javascript/operadores.js
@@ -1,24 +1,11 @@
-// operadores aritméticos
+// operadores de atribuição
+// = atribuição
 
-// adição +
-var somarDoisNumeros = 1 + 3;
-console.log(somarDoisNumeros);
-//subtração -
-var subtrairDoisNumeros = 1 - 3;
-console.log(subtrairDoisNumeros);
-//multiplicação *
-var multiplicarDoisNumeros = 1 * 3;
-console.log(multiplicarDoisNumeros);
-//divisão /
-var dividirDoisNumeros = 4 / 2;
-console.log(dividirDoisNumeros);
-//módulo ou resto de uma divisão -
-console.log(8%3);
-// incremento ++
-var numero = 1;
-console.log(++numero);
-numero = numero + 1
-// decremento --
-var numero = 2;
-console.log(--numero);
-numero = numero - 1
+// atribuição de soma
+var x = 1
+var y = 3
+console.log(x+=y);
+console.log(x-=y);
+console.log(x*=y);
+console.log(x/=y);
+console.log(x%=y);
