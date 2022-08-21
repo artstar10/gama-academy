7ba1980775af19917f6226e58cc2d940a81eb6c9
Arthur Neves <arthurnevesx@hotmail.com>
2022-08-21 15:19:06 -0300

    condicionais com switch case

--- dev-for-tech/back-end-nodejs/javascript/condicionais.js
+++ dev-for-tech/back-end-nodejs/javascript/condicionais.js
@@ -1,11 +1,16 @@
-var numero =1;
-numero = 5;
+var mes = "quero o mes de janeiro";
 
-if(numero === 1){
-    console.log("numero é igual a 1");
-}else if(numero === 2){
-    console.log("numero é igual a 2");
-}else{
-    console.log("numero é 5");
+switch (mes) {
+    case "janeiro":
+        console.log("mês 1");
+        break;
+    case "fevereiro":
+        console.log("mês 2");
+        break;
+    case "março":
+        console.log("mês 3");
+        break;
+    default:
+        console.log("nenhum dos casos acima foram atendidos");
+        break;
 }
-
